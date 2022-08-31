a0b562e6f49a431c8b62d0da933ae6cd62330cf3
mdc0 <68141024+mdc0@users.noreply.github.com>
2022-08-31 09:32:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -44,15 +44,15 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-def get_weChat():
-  client = WeChatClient(app_id, app_secret)
+# def get_weChat():
+client = WeChatClient(app_id, app_secret)
 
-  wm = WeChatMessage(client)
-  wea, temperature = get_weather()
-  data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-  res = wm.send_template(user_id, template_id, data)
-  print(res)
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
   
-scheduler = BlockingScheduler()
-scheduler.add_job(get_weChat, 'cron', second='*/5', args=['job1'])
-scheduler.start()
+# scheduler = BlockingScheduler()
+# scheduler.add_job(get_weChat, 'cron', second='*/5', args=['job1'])
+# scheduler.start()
