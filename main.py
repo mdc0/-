47cdd4c06ff77bc42c37972c0744d2cde0426223
mdc0 <68141024+mdc0@users.noreply.github.com>
2023-12-07 16:59:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-  print('天气'+str(words.json()))
+  print('文字'+str(words.json()['data']['text']))
   # if words.status_code != 200:
   #   return get_words()
   return words.json()['data']['text']
